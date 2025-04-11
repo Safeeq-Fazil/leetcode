b69375372722f91de6650385e29fb25b33cee47a
Safeeq-Fazil <118680361+Safeeq-Fazil@users.noreply.github.com>
2025-04-11 12:05:13 +0530

    Time: 2 ms (97.47%), Space: 41.5 MB (96.57%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> mystack = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{')
+            {
+                mystack.push(ch);
+            }
+            else
+            {
+                if(mystack.isEmpty()) return false;
+                else if(mystack.peek()=='(' && ch==')')
+                {
+                    mystack.pop();
+                }
+                else if(mystack.peek()=='[' && ch==']')
+                {
+                    mystack.pop();
+                }
+                else if(mystack.peek()=='{' && ch=='}')
+                {
+                    mystack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return mystack.isEmpty();
+    }
+}
