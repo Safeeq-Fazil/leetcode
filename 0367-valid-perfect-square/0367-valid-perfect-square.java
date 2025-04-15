09342d1514f938204a30f8721d580b1b7caab4bf
Safeeq-Fazil <118680361+Safeeq-Fazil@users.noreply.github.com>
2025-04-15 11:50:18 +0530

    Time: 0 ms (100%), Space: 40.8 MB (11.56%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(num==1) return true;
-        for(int i=1;i<=num/i;i++)
+        long start=1;
+        long end=num;
+        while(start<=end)
         {
-            if(i*i==num)
-            {
+            long mid=start+(end-start)/2;
+            if(mid*mid==num)
                 return true;
-            }
+            else if(mid*mid>num)
+                end=mid-1;
+            else
+                start=mid+1;        
         }
         return false;
         
