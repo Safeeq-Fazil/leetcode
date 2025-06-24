991e5716ed32ace44e64de1639e5b299e559a626
Safeeq-Fazil <118680361+Safeeq-Fazil@users.noreply.github.com>
2025-06-24 15:59:46 +0530

    Time: 0 ms (100%), Space: 43.1 MB (39.49%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,19 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left=0,right=nums.length-1,mid=0;
-        while(left<=right){
-            mid=left+(right-left)/2;
-            if(nums[mid]==target){
-                return mid;
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            if(nums[i]==target)
+            {
+                return i;
             }
-            else if(nums[mid]>target){
-                right=mid-1;
+            else if(nums[i]<target)
+            {
+                return i+1;
             }
-            else{
-                left=mid+1;
-            }
-
+            
         }
-        return left;
+        return 0;
     }
 }
