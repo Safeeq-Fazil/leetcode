fdbcd6c7a128fd4c6aa24f9dbc0475998da772d1
Safeeq-Fazil <118680361+Safeeq-Fazil@users.noreply.github.com>
2025-07-22 10:53:37 +0530

    Time: 0 ms (100%), Space: 41.2 MB (40.33%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,25 +15,14 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(p);
-        queue.add(q);
-        while(!queue.isEmpty())
+        if(p==null && q==null)
         {
-            TreeNode node1=queue.poll();
-            TreeNode node2=queue.poll();
-            if(node1==null && node2==null)
-            {
-                continue;
-            }
-            if(node1==null || node2==null || node1.val!=node2.val)
-                return false;
-            queue.add(node1.left);
-            queue.add(node2.left);
-            queue.add(node1.right);
-            queue.add(node2.right);    
-
+            return true;
         }
-        return true;
+        if(p==null || q==null || p.val!=q.val)
+        {
+            return false;
+        }
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
